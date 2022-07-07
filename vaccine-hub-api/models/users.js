f36b59cad34dfd0d5b69c1e040dee228dcecceb9
Ammar Fakih <ammar.fakih02@gmail.com>
2022-07-07 10:50:31 -0700

    finished

--- vaccine-hub-api/models/users.js
+++ vaccine-hub-api/models/users.js
@@ -1,14 +1,40 @@
 const db = require('../db');
 const bcrypt = require('bcrypt');
-const {BCRYPT_WORK_FACTOR} = require('../config');
+const { BCRYPT_WORK_FACTOR } = require('../config');
 const { BadRequestError, UnauthorizedError } = require('../utils/errors');
 
 class User {
+  static async makePublicUser(user) {
+    return {
+      id: user.id,
+      email: user.email,
+      firstName: user.first_name,
+      lastName: user.last_name,
+      location: user.location,
+    };
+  }
   static async login(credentials) {
+    const requiredFields = ['email', 'password'];
+    requiredFields.forEach((field) => {
+      if (!credentials.hasOwnProperty(field)) {
+        throw new BadRequestError(`Missing required field: ${field}`);
+      }
+    });
+
+    const user = await this.fetchUserByEmail(credentials.email);
+
+    if (user) {
+      const isValid = await bcrypt.compare(credentials.password, user.password);
+      if (isValid) {
+        return this.makePublicUser(user);
+      }
+    } else {
+      console.log('user not found');
+    }
+
     throw new UnauthorizedError('Invalid email/password combo');
   }
   static async register(credentials) {
-   
     const requiredFields = [
       'email',
       'password',
@@ -19,17 +45,21 @@ class User {
 
     requiredFields.forEach((field) => {
       if (!credentials.hasOwnProperty(field)) {
-        console.log(field)
+        console.log(field);
         throw new BadRequestError(`Missing required field: ${field}`);
       }
     });
 
     const existingUser = await this.fetchUserByEmail(credentials.email);
+    console.log('existingUser', existingUser);
     if (existingUser) {
       throw new BadRequestError(`Duplicate email: ${credentials.email}`);
     }
 
-    const hashedPassword = await bcrypt.hash(credentials.password, BCRYPT_WORK_FACTOR);
+    const hashedPassword = await bcrypt.hash(
+      credentials.password,
+      BCRYPT_WORK_FACTOR
+    );
 
     const lowerCasedEmail = credentials.email.toLowerCase();
 
@@ -50,7 +80,7 @@ class User {
 
     const user = result.rows[0];
 
-    return user;
+    return this.makePublicUser(user);
   }
 
   static async fetchUserByEmail(email) {
@@ -62,6 +92,8 @@ class User {
     const result = await db.query(query, [email.toLowerCase()]);
 
     const user = result.rows[0];
+
+    return user;
   }
 }
 
